644038829b4f5d10380c1912de7b01a9e7399f35
briskdust <briskdust@gmail.com>
2024-05-21 09:29:55 +0200

    refactor: Refactor `initialization.py`

--- initialization.py
+++ initialization.py
@@ -1,6 +1,10 @@
+"""
+Initializes the docker container for the Mobile Security Framework (MobSF) and runs it on port 8000.
+"""
+
 import subprocess
 
-command_str = "docker run -it --rm -p 8000:8000 opensecurity/mobile-security-framework-mobsf:latest"
+COMMAND_STR = "docker run -it --rm -p 8000:8000 opensecurity/mobile-security-framework-mobsf:latest"
 
-cmd = command_str.split(" ")
+cmd = COMMAND_STR.split(" ")
 subprocess.run(cmd, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
